043e9e79a4b22c5fcd0633dca8d23b3c7deb0344
kms902 <coldsilverblade@gmail.com>
2015-05-10 17:15:33 -0500

    Arrows are semi working now

--- new_python_game.py
+++ new_python_game.py
@@ -1,5 +1,11 @@
 # game_class_example.py
-# 
+# CURRENT - player box can move with arrow keys!
+# Working on - create bullet/arrow sprite that
+# fires linearly towards the mouse.
+# Problem: The good: Arrow fires away from the player and on a line towards the
+# mouse cursor. The bad: the arrow's speed changes based on the distance between
+# the mouse and the player, and the arrow even changes directions if the player
+# moves.
 
 import pygame
 import random
@@ -42,12 +48,34 @@ class Player(pygame.sprite.Sprite):
 		self.image = pygame.Surface([20,20])
 		self.image.fill(RED)
 		self.rect = self.image.get_rect()
+		self.change_x = 0
+		self.change_y = 0
 
 	def update(self):
 		""" Update player location """
-		pos = pygame.mouse.get_pos()
-		self.rect.x = pos[0]
-		self.rect.y = pos[1]
+		self.rect.x += self.change_x
+		self.rect.y += self.change_y
+
+	# Player-controlled movement:
+	def go_left(self):
+		""" Called when the user hits the left arrow. """
+		self.change_x = -3
+
+	def go_right(self):
+		self.change_x = 3
+
+	def go_up(self):
+		self.change_y = -3
+
+	def go_down(self):
+		self.change_y = 3
+
+	def stop_x(self):
+		""" Called when the user lets off the keyboard. """
+		self.change_x = 0
+	
+	def stop_y(self):
+		self.change_y = 0
 
 class Arrow(pygame.sprite.Sprite):
 	""" This class represents the bullet. """
@@ -55,14 +83,24 @@ class Arrow(pygame.sprite.Sprite):
 		# Call the parent class (Sprite) constructor
 		super().__init__()
 
-		self.image = pygame.Surface([4,10])
+		self.image = pygame.Surface([4,5])
 		self.image.fill(BLACK)
 
 		self.rect = self.image.get_rect()
 
+		# Get the current mouse position. This returns the position
+		# as a list of two numbers
+		self.pos = pygame.mouse.get_pos()
+
+		self.change_x = 0
+		self.change_y = 0
+
 	def update(self):
 		""" Move the bullet. """
-		pass
+		self.change_y = self.pos[1] - Game.player.rect.y
+		self.change_x = self.pos[0] - Game.player.rect.x 
+		self.rect.x += self.change_x
+		self.rect.y += self.change_y
 
 class Game(object):
 	""" This class represents an instance of the game. If we need to reset
@@ -74,6 +112,7 @@ class Game(object):
 		# Create sprite lists
 		self.block_list = pygame.sprite.Group()
 		self.all_sprites_list = pygame.sprite.Group()
+		self.arrow_list = pygame.sprite.Group()
 
 		# Create the block sprites
 		for i in range(50):
@@ -86,8 +125,17 @@ class Game(object):
 			self.all_sprites_list.add(block)
 
 		# Create the player
+		"""
+		# Huge problem was solved here
+		# Used to say:
 		self.player = Player()
+		but this was creating the player object on a scope too small to
+		use in other methods. I believe the scope became instance.
+		Whereas with Game.player = Player(), the scope became static variable. 
+		"""
+		Game.player = Player()
 		self.all_sprites_list.add(self.player)
+		self.arrow = Arrow()
 
 	def process_events(self):
 		""" Process all of the events. Return a "True" if we need to close the window. """
@@ -97,26 +145,35 @@ class Game(object):
 				return True
 			if event.type == pygame.KEYDOWN:
 				if event.key == pygame.K_LEFT:
-					Player.rect.x = -3
+					same.player.go_left()
 				if event.key == pygame.K_RIGHT:
-					Player.rect.x = 3
+					self.player.go_right()
 				if event.key == pygame.K_UP:
-					Player.rect.y = -3
+					self.player.go_up()
 				if event.key == pygame.K_DOWN:
-					Player.rect.y = 3
+					self.player.go_down()
 			if event.type == pygame.KEYUP:
 			# If it is an arrow key, reset vector back to zero
 				if event.key == pygame.K_LEFT:
-					Player.rect.x = 0
+					self.player.stop_x()
 				if event.key == pygame.K_RIGHT:
-					Player.rect.x = 0
+					self.player.stop_x()
 				if event.key == pygame.K_UP:
-					Player.rect.y = 0
+					self.player.stop_y()
 				if event.key == pygame.K_DOWN:
-					Player.rect.y = 0
+					self.player.stop_y()
 			if event.type == pygame.MOUSEBUTTONDOWN:
 				if self.game_over:
 					self.__init__()
+				else:
+					self.arrow = Arrow()
+					# Set the arrow so it is where the player is
+					self.arrow.rect.x = self.player.rect.x
+					self.arrow.rect.y = self.player.rect.y
+					# Add the bullet to the lists
+					self.all_sprites_list.add(self.arrow)
+					self.arrow_list.add(self.arrow)
+					#arrow_list.add(arrow)
 
 		return False
 
@@ -129,11 +186,15 @@ class Game(object):
 			# Move all the sprites
 			self.all_sprites_list.update()
 
-			# See if the player block has collided with anything and add collided blocks to a list
-			blocks_hit_list = pygame.sprite.spritecollide(self.player, self.block_list, True)
+			# See if the arrow block has collided with anything and add collided 
+			# blocks to a list. The dokill argument is a bool. If set to True, all 
+			# sprites that collide will be removed from the Group.
+			blocks_hit_list = pygame.sprite.spritecollide(self.arrow, self.block_list, True)
 
 			# Check the list of collisions.
 			for block in blocks_hit_list:
+				self.arrow_list.remove(self.arrow)
+				self.all_sprites_list.remove(self.arrow)
 				self.score += 1
 				print(self.score)
 				# You can do something with "block" here
@@ -167,7 +228,7 @@ def main():
 	screen = pygame.display.set_mode(size)
 
 	pygame.display.set_caption("Simple RPG")
-	pygame.mouse.set_visible(False)
+	pygame.mouse.set_visible(True)
 
 	# Create our objects and set the data
 	done = False
